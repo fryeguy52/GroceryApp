51d3c66cdc2df2da4a27060d1d39c921087fff2d
Joe Frye <fryeguy52@gmail.com>
2017-09-05 21:30:22 -0600

    ? updates ?

--- grocery_options.py
+++ grocery_options.py
@@ -1,33 +1,57 @@
 __author__ = 'Joe'
+JTxCan_Albertsons=["bread",
+              "crackers",
+              "meat",
+              "alcohol",
+              "cheese",
+              "tortillas",
+              "soda",
+              "frozen",
+              "coffee",
+              "cereal",
+              "baking",
+              "canned",
+              "soup",
+              "pasta",
+              "chips",
+              "international",
+              "condiments",
+              "spices",
+              "dairy",
+              "root",
+              "vegetable",
+              "fruit",
+              "produce"]
+
+MoxTr_Smiths=["bakery",
+              "liquor",
+              "fruit",
+              "root",
+              "vegetable",
+              "Frozen",
+              "tortillas",
+              "condiments",
+              "bread",
+              "international",
+              "canned",
+              "pasta",
+              "baking",
+              "spices",
+              "meat",
+              "cereal",
+              "cheese",
+              "dairy",
+              "juice",
+              "soda",
+              "snacks"]
+
 
 class grocery_options:
     def __init__(self):
         self.include_tags=[]
         self.exclude_tags=["hide"]
         self.default_recipes=["Grocery Staples"]
-        self.print_order=["bread",
-                          "crackers",
-                          "meat",
-                          "alcohol",
-                          "cheese",
-                          "tortillas",
-                          "soda",
-                          "frozen",
-                          "coffee",
-                          "cereal",
-                          "baking",
-                          "canned",
-                          "soup",
-                          "pasta",
-                          "chips",
-                          "international",
-                          "condiments",
-                          "spices",
-                          "dairy",
-                          "root",
-                          "vegetable",
-                          "fruit",
-                          "produce"]
+        self.print_order=MoxTr_Smiths
 
         # debug_show_recipe_list
         # debug_make_grocery_list
